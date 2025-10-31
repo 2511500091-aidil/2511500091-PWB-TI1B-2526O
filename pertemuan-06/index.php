546b030486ac115e1965e8e394ea80f912b4dc48
Muhammad Aidil Zaky <2511500091@mahasiswa.atmaluhur.ac.id>
2025-10-31 20:48:49 +0700

    Variabel php ke section#about

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -18,38 +18,49 @@
       </ul>
     </nav>
   </header>
+
   <main>
     <section id="home">
       <h2>Selamat Datang</h2>
       <p>Ini contoh paragraf HTML.</p>
       <?php
-      echo"Halo Dunia";
-      echo"Nama Saya Muhammad Aidil Zaky";
+        echo "<p>Halo Dunia!</p>";
+        echo "<p>Nama Saya Muhammad Aidil Zaky</p>";
       ?>
     </section>
+
+    <?php
+      $nim             = "2511500091";
+      $nama_lengkap    = "Muhammad Aidil Zaky";
+      $tempat_lahir    = "Pangkalpinang";
+      $tanggal_lahir   = "13 Oktober 2007";
+      $hobi            = "Jual beli COD dan akun game 🎮, serta mempelajari hal-hal baru tentang teknologi 💻 dan dunia digital 🌐";
+      $pasangan        = "Elsya Vebrianti – pasangan terbaik yang saya temui sejak SMK kelas 10, dan semoga nanti bisa jadi pasangan hidup saya ❤️🔥";
+      $pekerjaan       = "Mahasiswa";
+      $nama_ayah       = "Wandy Aprizal";
+      $nama_ibu        = "Denti Seprianti";
+      $adik_laki       = "Faris Fatih";
+      $adik_perempuan  = "Aura Aliana";
+    ?>
+
     <section id="about">
-        <?php
-        $NIM = "2511500091";
-        $nim = "2511500090";
-        $nim = "";
-        ?>
       <h2>Tentang Saya</h2>
-      <p><strong>NIM:</strong> 2511500091</p>
-      <p><strong>Nama Lengkap:</strong> Muhammad Aidil Zaky&#128526;</p>
-      <p><strong>Tempat Lahir:</strong> Pangkalpinang</p>
-      <p><strong>Tanggal Lahir:</strong> 13 Oktober 2007</p>
-      <p><strong>Hobi:</strong> Jual beli COD dan akun game 🎮, serta mempelajari hal-hal baru tentang teknologi 💻 dan dunia digital 🌐</p>
-      <p><strong>Pasangan:</strong> Elsya Vebrianti – pasangan terbaik yang saya temui sejak SMK kelas 10, dan semoga nanti bisa jadi pasangan hidup saya ❤️🔥;</p>
-      <p><strong>Pekerjaan:</strong> Mahasiswa</p>
-      <p><strong>Nama Ayah:</strong> Wandy Aprizal</p>
-      <p><strong>Nama Ibu:</strong> Denti Seprianti</p>
-      <p><strong>Nama Adik Laki-Laki:</strong> Faris Fatih</p>
-      <p><strong>Nama Adik Perempuan:</strong> Aura Aliana</p>
+      <p><strong>NIM:</strong> <?= $nim; ?></p>
+      <p><strong>Nama Lengkap:</strong> <?= $nama_lengkap; ?> 😎</p>
+      <p><strong>Tempat Lahir:</strong> <?= $tempat_lahir; ?></p>
+      <p><strong>Tanggal Lahir:</strong> <?= $tanggal_lahir; ?></p>
+      <p><strong>Hobi:</strong> <?= $hobi; ?></p>
+      <p><strong>Pasangan:</strong> <?= $pasangan; ?></p>
+      <p><strong>Pekerjaan:</strong> <?= $pekerjaan; ?></p>
+      <p><strong>Nama Ayah:</strong> <?= $nama_ayah; ?></p>
+      <p><strong>Nama Ibu:</strong> <?= $nama_ibu; ?></p>
+      <p><strong>Nama Adik Laki-Laki:</strong> <?= $adik_laki; ?></p>
+      <p><strong>Nama Adik Perempuan:</strong> <?= $adik_perempuan; ?></p>
     </section>
+
     <section id="contact">
       <h2>Kontak Kami</h2>
       <form action="" method="GET">
-
         <label for="txtNama"><span>Nama:</span>
           <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
         </label>
@@ -68,9 +79,11 @@
       </form>
     </section>
   </main>
+
   <footer>
-    <p>&copy; Muhammad Aidil Zaky [2511500091}</p>
+    <p>&copy; <?= $nama_lengkap; ?> [<?= $nim; ?>]</p>
   </footer>
-    <script src="script.js"></script>
+
+  <script src="script.js"></script>
 </body>
 </html>
